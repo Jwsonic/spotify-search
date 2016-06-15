221baabc4d1ce902c2ba1c7823666c74ea642eef
John Palgut <jwsonic@gmail.com>
2016-06-15 13:06:58 -0700

    removing some comments

--- src/main.elm
+++ src/main.elm
@@ -20,6 +20,10 @@ main =
     program { init = init, update = update, subscriptions = (\_ -> Sub.none), view = view }
 
 
+
+-- Spotify data types
+
+
 type alias SpotifyAlbum =
     { name : String
     , images : List String
@@ -168,43 +172,6 @@ trackView track =
                 , div [ class "subtitle is-6" ] [ text artist ]
                 ]
             ]
-
-
-
--- div [ class "card" ]
---     [ div [ class "card-image" ]
---         [ figure [ class "image is-128x128" ] [ img [ src "http://placehold.it/300x225" ] [] ]
---         ]
---     , div [ class "card-content" ] [ text track.name ]
---     ]
---   <div class="card">
---   <div class="card-image">
---     <figure class="image is-128x128">
---       <img src="http://placehold.it/300x225" alt="">
---     </figure>
---   </div>
---   <div class="card-content">
---     <div class="media">
---       <div class="media-left">
---         <figure class="image is-32x32">
---           <img src="http://placehold.it/64x64" alt="Image">
---         </figure>
---       </div>
---       <div class="media-content">
---         <p class="title is-5">John Smith</p>
---         <p class="subtitle is-6">@johnsmith</p>
---       </div>
---     </div>
---
---     <div class="content">
---       Lorem ipsum dolor sit amet, consectetur adipiscing elit.
---       Phasellus nec iaculis mauris. <a href="#">@bulmaio</a>.
---       <a href="#">#css</a> <a href="#">#responsive</a>
---       <br>
---       <small>11:09 PM - 1 Jan 2016</small>
---     </div>
---   </div>
--- </div>
 
 
 decodeSpotifyJson : Decoder TrackList
